ac1015c68b77c238accb7b43b31a89b19a09b815
nihalarju <nihalarju@gmail.com>
2015-07-12 10:16:19 -0500

    Update median_unique.py

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,16 +1,64 @@
 #! /usr/bin/python
 
+# This program reads in a text file line by line, and finds the median
+# number of unique words per line. 
+
+# We do not use numpy's median function because that's not the fastest way
+# to do this. Instead we populate list of numbers in ascending order.
+
 import sys
 f=open(sys.argv[1],'r')
 fout=open(sys.argv[2],'w')
 
-#f=open('../bup/book1.txt','r')
-#fout=open('../tweet_output/ft2_new.txt','w')
+#=============================================================================
+def insertInOrder(sortedlist,num):
+    # Given an sorted list (sortedlist), and a number (num), this subroutine
+    # will insert the number in right order.
+    # To achieve heighest possible speed, the program does not check the 
+    # sortedlist for its ordering. Careful.
+    
+    # How it works:
+    # The program tries to locate the place to put the number (num) by
+    # trying to find smaller and smaller pieces that bound the value (num)
+    # Once the piece is small enough, just a brute force search is used.    
+    length=len(sortedlist)
+    inc_lst=[]
+    l=1
+    r=length-2
+    if length<1 or num>=sortedlist[length-1]:
+        sortedlist.append(num)
+    elif num<=sortedlist[0]:
+        sortedlist.insert(0,num)
+    else:
+        inc=length//8+1
+        while inc>3: # Find piece of size about 4*2=8 numbers
+            inc_lst.append(inc)
+            # Find the left horizon
+            while l+inc < length-1 and num >= sortedlist[l+inc]: 
+                l+=inc 
+            # Find the right side horizon
+            while r-inc > 0 and num <= sortedlist[r-inc]:
+                r-=inc
+            if sortedlist[l]==num:
+                sortedlist.insert(l,num)
+                break
+            if sortedlist[r]==num:
+                sortedlist.insert(r,num)
+                break
+            inc=inc//2+1
+    # Now, unless the left and right horizon included (num), we still need
+    # to insert num. Now the task is much simpler, because we only have about
+    # 8 numbers to go through.
+    if len(sortedlist)==length:
+        i=l
+        while num > sortedlist[i]:
+                i+=1
+        sortedlist.insert(i,num)
+    return sortedlist
+#=============================================================================
 
 sortedlist=[] # List of numbers- number of unique words in a tweet
-writestr=str()
 numlines=0
-medval=0
 #from numpy import median
 
 for line in f:
@@ -31,34 +79,7 @@ for line in f:
     # Since we populate the list of numbers, we can populate it
     # to be in ascending order. Therefore reducing the
     # computational load.
-    # THIS LOOKS AWFUL, but THIS IS FAST!
-    if numlines<10:
-        sortedlist.append(numunique)
-        sortedlist.sort()
-    else:
-        length=len(sortedlist)
-        if numunique==medval:
-            sortedlist.insert( (length)//2, numunique)
-        else:
-            if numunique>medval:
-                if numunique>=sortedlist[length-1]:
-                    sortedlist.append(numunique)
-                else:
-                    indices=range(length//2,length)
-                    for i in indices:
-                        if numunique<sortedlist[i]:
-                            sortedlist.insert(i,numunique)
-                            break
-            else:
-                if numunique<=sortedlist[0]:
-                    sortedlist.insert(0,numunique)
-                else:
-                    indices=range(0,length//2)
-                    indices.reverse()
-                    for i in indices:
-                        if numunique>sortedlist[i]:
-                            sortedlist.insert(i+1,numunique)
-                            break
+    insertInOrder(sortedlist,numunique)
 
     # After populating the list of number of unique words, we find the median
     if numlines %2 == 1:
